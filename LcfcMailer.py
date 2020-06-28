ecf66184fc1867e7a78c4580232d25fbc3fc7141
andy1990zx <andy1990zx@163.com>
2020-06-28 14:49:43 +0800

    Update license to BSD 2-Clause
    Update license to BSD 2-Clause

--- LcfcMailer.py
+++ LcfcMailer.py
@@ -1,11 +1,22 @@
 # coding:utf-8
 """
-*****************************************************************************
-*  Copyright (c) 2012 - 2020, Hefei LCFC Information Technology Co.Ltd.
-*  And/or its affiliates. All rights reserved.
-*  Hefei LCFC Information Technology Co.Ltd. PROPRIETARY/CONFIDENTIAL.
-*  Use is subject to license terms.
-*****************************************************************************
+BSD 2-Clause License
+
+Copyright (c) 2020, Sea Zhou, WeChat: uefi64
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+"""
+
+"""
 Name        : LcfcMailer.py
 Usage       : LcfcMailer.py (It will fetch MailList.xlsx in the same folder)
 EXE Package : pyinstaller -F -i LCFC.ico LcfcMailer.py
